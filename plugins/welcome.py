441e549b35540f1429079cc37d30c4894224db6f
Lallu-lallus <89923355+Lallu-lallus@users.noreply.github.com>
2021-12-06 19:44:01 +0530

    🙂

--- plugins/welcome.py
+++ plugins/welcome.py
@@ -7,7 +7,7 @@ from pyrogram.types import Message, User
 @Client.on_message(filters.new_chat_members)
 async def welcome(bot,message):
 	chatid= message.chat.id
-	await bot.send_message(text=f"Welcome {message.from_user.mention} to {message.chat.username} ,  ʙʀᴜʜ ᴛʜɪs ɪs ᴀ ᴍᴜsɪᴄ ɢʀᴏᴜᴘ ᴅᴏɴᴛ ᴀsᴋ ᴍᴏᴠɪᴇs ʜᴇʀᴇ ᴅᴏɴᴛ ᴜsᴇ sᴘᴀᴍ ᴡᴏʀᴅs ᴀsᴋ sᴏɴɢ ɴᴀᴍᴇ ᴡɪᴛʜ ᴄᴏᴍᴍᴀɴᴅ 🙂 ᴇɢ:/song faded, ᴅᴏᴡɴʟᴏᴀᴅ ʏᴛ ᴠɪᴅᴇᴏs🙂 eg: /video yt_link ᴏᴋ ʙʀᴏ ᴇɴᴊᴏʏ ᴛʜᴇ ᴜɴʟɪᴍɪᴛᴇᴅ ᴍᴜsɪᴄs🙂",chat_id=chatid)
+	await bot.send_message(text=f"Welcome {message.from_user.mention} to {message.chat.username} ,  ʙʀᴜʜ ᴛʜɪs ɪs ᴀ ᴍᴜsɪᴄ ɢʀᴏᴜᴘ ᴅᴏɴᴛ ᴀsᴋ ᴍᴏᴠɪᴇs ʜᴇʀᴇ ᴅᴏɴᴛ ᴜsᴇ sᴘᴀᴍ ᴡᴏʀᴅs ᴀsᴋ sᴏɴɢ ɴᴀᴍᴇ ᴡɪᴛʜ ᴄᴏᴍᴍᴀɴᴅ 🙂 ᴇɢ:/song faded or /s faded, ᴅᴏᴡɴʟᴏᴀᴅ ʏᴛ ᴠɪᴅᴇᴏs🙂 eg: /video yt_link or /v  yt_link ᴏᴋ ʙʀᴏ ᴇɴᴊᴏʏ ᴛʜᴇ ᴜɴʟɪᴍɪᴛᴇᴅ ᴍᴜsɪᴄs🙂",chat_id=chatid)
 	
 @Client.on_message(filters.left_chat_member)
 async def goodbye(bot,message):
