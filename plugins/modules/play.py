335f618bf464a579b67564a8e31522a4b94956d3
Lallu-lallus <89923355+Lallu-lallus@users.noreply.github.com>
2021-12-07 07:25:53 +0530

    Update play.py

--- plugins/modules/play.py
+++ plugins/modules/play.py
@@ -1,3 +1,20 @@
+import os
+from os import path
+from typing import Callable
+
+import aiofiles
+import aiohttp
+import ffmpeg
+import requests
+import wget
+from PIL import Image, ImageDraw, ImageFont
+from pyrogram import Client, filters
+from pyrogram.errors import UserAlreadyParticipant
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+from Python_ARQ import ARQ
+from youtube_search import YoutubeSearch
+
+
 aiohttpsession = aiohttp.ClientSession()
 chat_id = None
 arq = ARQ("https://thearq.tech", ARQ_API_KEY, aiohttpsession)
