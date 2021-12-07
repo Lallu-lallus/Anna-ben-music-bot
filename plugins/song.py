a1a3fe5343dfb0d0edd77b3d59c5939fcdd49ea3
Lallu-lallus <89923355+Lallu-lallus@users.noreply.github.com>
2021-12-07 21:05:31 +0530

    🙂

--- plugins/song.py
+++ plugins/song.py
@@ -30,11 +30,13 @@ def time_to_seconds(time):
 ## Commands --------------------------------
 
 
-@Client.on_message(filters.command(["song", "s"]))
+@Bot.on_message(filters.command(['song']))
 def a(client, message):
-    query=message.text
+    query = ''
+    for i in message.command[1:]:
+        query += ' ' + str(i)
     print(query)
-    m = message.reply('🔎 𝗦𝗲𝗮𝗿𝗰𝗵𝗶𝗻𝗴 𝘁𝗵𝗲 𝗦𝗼𝗻𝗴...')
+    m = message.reply('`🔎𝑺𝒆𝒂𝒓𝒄𝒉𝒊𝒏𝒈....')
     ydl_opts = {"format": "bestaudio[ext=m4a]"}
     try:
         results = []
@@ -51,42 +53,43 @@ def a(client, message):
             title = results[0]["title"]
             thumbnail = results[0]["thumbnails"][0]
             duration = results[0]["duration"]
+            views = results[0]["views"]
 
             ## UNCOMMENT THIS IF YOU WANT A LIMIT ON DURATION. CHANGE 1800 TO YOUR OWN PREFFERED DURATION AND EDIT THE MESSAGE (30 minutes cap) LIMIT IN SECONDS
-            # if time_to_seconds(duration) >= 1800:  # duration limit
+            # if time_to_seconds(duration) >= 7000:  # duration limit
             #     m.edit("Exceeded 30mins cap")
             #     return
 
-            views = results[0]["views"]
+            performer = f"[@music_wrld_grp]" 
             thumb_name = f'thumb{message.message_id}.jpg'
             thumb = requests.get(thumbnail, allow_redirects=True)
             open(thumb_name, 'wb').write(thumb.content)
 
         except Exception as e:
             print(e)
-            m.edit('𝐅𝐨𝐮𝐧𝐝 𝐍𝐨𝐭𝐡𝐢𝐧𝐠. 𝐓𝐫𝐲 𝐂𝐡𝐚𝐧𝐠𝐢𝐧𝐠 𝐓𝐡𝐞 𝐒𝐩𝐞𝐥𝐥𝐢𝐧𝐠 𝐀 𝐋𝐢𝐭𝐭𝐥𝐞 😐')
+            m.edit('**No Results Found With This Data!**')
             return
     except Exception as e:
         m.edit(
-            "❎ 𝐹𝑜𝑢𝑛𝑑 𝑁𝑜𝑡ℎ𝑖𝑛𝑔. 𝐒𝐨𝐫𝐫𝐲.\n\n𝖯𝗅𝖾𝖺𝗌𝖾 𝖳𝗋𝗒 𝖠𝗀𝖺𝗂𝗇 𝖮𝗋 𝖲𝖾𝖺𝗋𝖼𝗁 𝖺𝗍 Google.com 𝖥𝗈𝗋 𝖢𝗈𝗋𝗋𝖾𝖼𝗍 𝖲𝗉𝖾𝗅𝗅𝗂𝗇𝗀 𝗈𝖿 𝗍𝗁𝖾 𝙎𝙤𝙣𝙜.\n\nEg.`Believer`"
+            "**Enter The Song Name with /song command.!**"
         )
         print(str(e))
         return
-    m.edit("`🎸𝐼 𝐹𝑜𝑢𝑛𝑑 𝑎 𝑠𝑜𝑛𝑔 𝑝𝑙𝑒𝑎𝑠𝑒 𝑤𝑎𝑖𝑡𝑒 𝐼 𝑢𝑝𝑙𝑜𝑎𝑑𝑖𝑛𝑔 𝑖𝑡.....`")
+    m.edit("`🎸𝐼 𝐹𝑜𝑢𝑛𝑑 𝑎 𝑠𝑜𝑛𝑔 𝑝𝑙𝑒𝑎𝑠𝑒 𝑤𝑎𝑖𝑡𝑒 𝑨𝒎 𝑢𝑝𝑙𝑜𝑎𝑑𝑖𝑛𝑔 𝑖𝑡.....`")
     try:
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep =  f'🎧 𝗧𝗶𝘁𝘁𝗹𝗲 : [{title[:35]}]\n⏳ 𝗗𝘂𝗿𝗮𝘁𝗶𝗼𝗻 : `{duration}`\n👀 𝗩𝗶𝗲𝘄𝘀 : `{views}`\n\n📮 𝗕𝘆: {message.from_user.mention()}\n📤 𝗕𝘆 : @Movie_wrld_grp'
+        rep = f'❍📖 <b>ᴛɪᴛʟᴇ:</b> <a href="{link}">{title}</a>\n❍⌚ <b>ᴅᴜʀᴀᴛɪᴏɴ:</b> <code>{duration}</code>\n❍📤 <b>ᴜᴘʟᴏᴀᴅᴇᴅ ʙʏ:</b>@music_wrld_grp'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
-            dur += (int(dur_arr[i]) * secmul)
+            dur += (int(dur_arr[i]) * secmul)<
             secmul *= 60
-        message.reply_audio(audio_file, caption=rep, parse_mode='HTML',quote=False, title=title, duration=dur, performer=str(info_dict["uploader"]), thumb=thumb_name)
+        message.reply_audio(audio_file, caption=rep, parse_mode='HTML',quote=False, title=title, duration=dur, performer=performer, thumb=thumb_name)
         m.delete()
     except Exception as e:
-        m.edit('𝙁𝙖𝙞𝙡𝙚𝙙\n\n`Plesase Try Again Later`')
+        m.edit('**Something Went Wrong Report This at @NAZRIYASUPPORT!!**')
         print(e)
     try:
         os.remove(audio_file)
