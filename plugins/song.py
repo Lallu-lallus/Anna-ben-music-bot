a06117836dc4dfeab5e648087b29255f8e3aad8f
Lallu-lallus <89923355+Lallu-lallus@users.noreply.github.com>
2021-12-07 07:22:31 +0530

    Update song.py

--- plugins/song.py
+++ plugins/song.py
@@ -15,6 +15,8 @@ from youtubesearchpython import SearchVideos
 from yt_dlp import YoutubeDL
 import youtube_dl
 from youtube_search import YoutubeSearch
+from plugins.modules.play import arq
+
 import requests
 
 ## Extra Fns -------------------------------
