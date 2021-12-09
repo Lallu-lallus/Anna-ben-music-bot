8f37ae03c7673492c7bfa8a27b87fd3d7060ee2b
Lallu-lallus <89923355+Lallu-lallus@users.noreply.github.com>
2021-12-09 19:22:19 +0530

    Update and rename info.pu to info.py

---
+++ info.py
@@ -0,0 +1,15 @@
+import re
+from os import environ
+
+id_pattern = re.compile(r'^.\d+$')
+def is_enabled(value, default):
+    if value.lower() in ["true", "yes", "1", "enable", "y"]:
+        return True
+    elif value.lower() in ["false", "no", "0", "disable", "n"]:
+        return False
+    else:
+        return default
+
+# MongoDB information
+DATABASE_URI = environ.get('DATABASE_URI', "")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Rajappan")
