811f5c0103076ec8566a5264f516c10ac606105d
Lallu-lallus <89923355+Lallu-lallus@users.noreply.github.com>
2021-12-09 20:30:22 +0530

    Create util.py

---
+++ util.py
@@ -0,0 +1,16 @@
+def get_file_id(msg: Message):
+    if msg.media:
+        for message_type in (
+            "photo",
+            "animation",
+            "audio",
+            "document",
+            "video",
+            "video_note",
+            "voice",
+            "sticker"
+        ):
+            obj = getattr(msg, message_type)
+            if obj:
+                setattr(obj, "message_type", message_type)
+                return obj
