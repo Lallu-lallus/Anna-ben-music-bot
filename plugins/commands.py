696e8074be5f6ca343bed7f08e478399b9ba9816
Lallu-lallus <89923355+Lallu-lallus@users.noreply.github.com>
2021-12-16 17:06:28 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -54,7 +54,7 @@ async def start(client, message):
     ]
     reply_markup = InlineKeyboardMarkup(buttons)
     await message.reply(HOME_TEXT.format(message.from_user.first_name, message.from_user.id), reply_markup=reply_markup)
-    await message.reply_photo(photo=random.choice(PIC)
+    await message.reply_photo(photo=random.choice(PIC),
 
 
 @Client.on_message(filters.command("help"))
